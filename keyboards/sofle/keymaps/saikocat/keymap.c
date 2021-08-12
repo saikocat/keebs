c86964a54c5c72a717b3e0a4bb0e80cd2c699591
Hoa <hoameomu@gmail.com>
2021-08-12 17:15:34 +0800

    [sofle] use hold lower and raise to goto adjust layer

--- keyboards/sofle/keymaps/saikocat/keymap.c
+++ keyboards/sofle/keymaps/saikocat/keymap.c
@@ -34,6 +34,10 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format on
 };
 
+/* Hold _LOWER and _RAISE set to _ADJUST */
+layer_state_t layer_state_set_user(layer_state_t state) {
+    return update_tri_layer_state(state, _LOWER, _RAISE, _ADJUST);
+}
 
 /* Function: Bit-C microcontroller specific
  * ----------------------------------------
