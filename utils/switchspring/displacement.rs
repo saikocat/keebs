f4962b71b006f6f4da983070b865b652ebf9146c
Hoa <hoameomu@gmail.com>
2021-08-23 23:56:08 +0800

    Add switch displacement constants for known mx clones

---
+++ utils/switchspring/displacement.rs
@@ -0,0 +1,27 @@
+/// Switch Displacement in mm
+pub struct SwitchDisplacement {
+    _actuation: f32,
+    _bottom_out: f32,
+}
+
+impl SwitchDisplacement {
+    pub const fn new(actuation: f32, bottom_out: f32) -> SwitchDisplacement {
+        SwitchDisplacement {
+            _actuation: actuation,
+            _bottom_out: bottom_out,
+        }
+    }
+
+    pub const fn actuation(&self) -> f32 {
+        self._actuation
+    }
+
+    pub const fn bottom_out(&self) -> f32 {
+        self._bottom_out
+    }
+}
+
+const CHERRY_MX: SwitchDisplacement = SwitchDisplacement::new(2.0, 4.0);
+const CHERRY_MX_SPEED: SwitchDisplacement = SwitchDisplacement::new(1.2, 3.4);
+const GATERON_INK_YELLOW: SwitchDisplacement = SwitchDisplacement::new(1.5, 3.5);
+const KAILH_SPEED: SwitchDisplacement = SwitchDisplacement::new(1.1, 3.5);
