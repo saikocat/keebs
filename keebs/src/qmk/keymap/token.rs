935e479809f9a6b4346bbe7bf30123234619f353
Hoa <hoameomu@gmail.com>
2021-09-01 21:21:09 +0800

    [qmk keymap] add more punctuation and identifier tokens

--- keebs/src/qmk/keymap/token.rs
+++ keebs/src/qmk/keymap/token.rs
@@ -5,9 +5,14 @@ pub mod punctuation {
     pub const CLOSING_PARENTH: &'static str = ")";
     pub const PIPE: &'static str = "|";
     pub const COMMA: &'static str = ",";
+    pub const BACKSLASH: &'static str = "\\";
+    pub const UNDERSCORE: &'static str = "_";
 }
 
 pub mod identifier {
     pub const KEYCODE_NOT_STARTSWITH: &'static str = "|0123456789";
-    pub const KEYCODE_ONEOF: &'static str = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
+    pub const KEYCODE_ONEOF: &'static str = "0123456789_";
+    pub const KEYCODE_MACRO_NONEOF: &'static str = ",\\";
+    pub const ACCEPTABLE_CHAR: &'static str =
+        "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_";
 }
