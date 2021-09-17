6c9464167e532ddcda07c8786bd3ccd3cd85698c
Hoa <hoameomu@gmail.com>
2021-09-17 15:23:37 +0800

    [qmk userspace] enable LTO

--- qmk_userspace/saikocat/rules.mk
+++ qmk_userspace/saikocat/rules.mk
@@ -1,1 +1,8 @@
 SRC += saikocat.c
+
+# Enable Link Time Optimization wherever supported
+ifneq ($(PLATFORM),CHIBIOS)
+    ifneq ($(strip $(LTO_SUPPORTED)), no)
+        LTO_ENABLE = yes
+    endif
+endif
