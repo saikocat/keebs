6545a7aed2be778e83d7ec53c6453e6ac886df2b
Hoa <hoameomu@gmail.com>
2021-09-25 23:03:10 +0800

    [qmk uspc] add rotary encoder features

---
+++ qmk_userspace/saikocat/encoder_custom.c
@@ -0,0 +1,144 @@
+/* Copyright (C) 2021 @saikocat
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/* Function: encoder
+ * -----------------
+ * A rotary encoder is a device that you can twist to perform actions.
+ * Some encoders can also be pushed, just like a button!
+ * A rotary encoder twists clockwise and counter clockwise.
+ * It lends itself to actions that you'd otherwise need to repeatedly press a button for,
+ * and to actions that have both a forward and a reverse direction.
+ *
+ * Reference: https://docs.splitkb.com/hc/en-us/articles/360010513760-How-can-I-use-a-rotary-encoder-
+ */
+
+#include "encoder.h"
+
+/* Audio control */
+void encoder_fn_audio_control(bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_VOLU);
+    } else {
+        tap_code(KC_VOLD);
+    }
+}
+
+/* Scrolling */
+void encoder_fn_scrolling(bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_PGDOWN);
+    } else {
+        tap_code(KC_PGUP);
+    }
+}
+
+/* Tabbing
+ * -------
+ * Moving through browser mimicing Control + Tab and Control + Shift + Tab
+ */
+void encoder_fn_tabbing(bool clockwise) {
+    if (clockwise) {
+        tap_code16(C(KC_TAB));
+    } else {
+        tap_code16(S(C(KC_TAB)));
+    }
+}
+
+/* History Scrubbing
+ * -----------------
+ * This will perform Control + Z when you turn the encoder clockwise,
+ * and Control + Y when turning it counterclockwise.
+ * With this, you can easily "scroll" through the history when editing a document.
+ */
+void encoder_fn_history_scrubbing(bool clockwise) {
+    if (clockwise) {
+        tap_code16(C(KC_Y));
+    } else {
+        tap_code16(C(KC_Z));
+    }
+}
+
+/* Scrolling Horizontally by Word
+ * ------------------------------
+ * This will perform Control + Right Arrow when you turn the encoder clockwise,
+ * and Control + Left Arrow when turning it counterclockwise.
+ * If you hold shift while turning the encoder, you'll be able to select words while the cursor moves!
+ */
+void encoder_fn_word_scrolling(bool clockwise) {
+    if (clockwise) {
+        tap_code16(C(KC_RGHT));
+    } else {
+        tap_code16(C(KC_LEFT));
+    }
+}
+
+/* Scrolling Through Search Results
+ * --------------------------------
+ * Shortcuts to move to the next or previous result. F3 and Shift + F3.
+ */
+void encoder_fn_search_through_results(bool clockwise) {
+    if (clockwise) {
+        tap_code(KC_F3);
+    } else {
+        tap_code16(S(KC_F3));
+    }
+}
+
+/* Window tabbing
+ * --------------------------------
+ * Like with tabs, you can also move through applications.
+ * In Windows, you can do this with Alt + Tab and Alt + Shift + Tab.
+ */
+void encoder_fn_wins_tabbing(bool clockwise) {
+    extern bool     is_alt_tab_active;
+    extern uint16_t alt_tab_timer;
+
+    if (clockwise) {
+        if (!is_alt_tab_active) {
+            is_alt_tab_active = true;
+            register_code(KC_LALT);
+        }
+        alt_tab_timer = timer_read();
+        tap_code16(KC_TAB);
+    } else {
+        alt_tab_timer = timer_read();
+        tap_code16(S(KC_TAB));
+    }
+}
+
+bool     is_alt_tab_active = false;
+uint16_t alt_tab_timer     = 0;
+
+__attribute__((weak)) void matrix_scan_encoder(void) {
+    if (is_alt_tab_active) {
+        if (timer_elapsed(alt_tab_timer) > 1250) { /* 1250ms hold wait */
+            unregister_code(KC_LALT);
+            is_alt_tab_active = false;
+        }
+    }
+}
+
+__attribute__((weak)) bool encoder_update_keymap(uint8_t index, bool clockwise) {
+    if (index == 0) {
+        encoder_fn_wins_tabbing(clockwise);
+    } else if (index == 1) {
+        encoder_fn_scrolling(clockwise);
+    }
+    return true;
+}
+
+/* User defined func that will be called by QMK every time encoder is turned */
+bool encoder_update_user(uint8_t index, bool clockwise) { return encoder_update_keymap(index, clockwise); }
