4d50da2a23fbf4a81fda14c2b87482171c953a64
Hoa <hoameomu@gmail.com>
2021-10-09 20:01:24 +0800

    [qmk layout] update solfe layout

--- qmk_userspace/saikocat/layouts/sofle.h
+++ qmk_userspace/saikocat/layouts/sofle.h
@@ -129,9 +129,9 @@
 #define __LLY__ADJUST \
     XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,                      XXXXXXX,         XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, \
     _______, KC_PAST, KC_7,    KC_8,    KC_9,    KC_PMNS,                      DF(_QWERTY),     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_BSPC, \
-    _______, KC_PEQL, KC_4,    KC_5,    KC_6,    KC_PPLS,                      DF(_COLEMAK_DH), KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, \
+    _______, KC_EQL,  KC_4,    KC_5,    KC_6,    KC_PPLS,                      DF(_COLEMAK_DH), KC_F4,   KC_F5,   KC_F6,   KC_F11,  _______, \
     _______, KC_P0,   KC_1,    KC_2,    KC_3,    KC_PSLS, XXXXXXX,    XXXXXXX, DF(_RAISE),      KC_F1,   KC_F2,   KC_F3,   KC_F12,  KC_LGUI, \
-                  _______, _______,  KC_0,       _______, _______,    _______, _______, _______, _______, _______
+                  _______, _______,  KC_PDOT,    _______, _______,    _______, _______, _______, _______, _______
 
 
 /*
